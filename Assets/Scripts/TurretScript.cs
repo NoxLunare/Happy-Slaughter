0e76d8d52f40ed8fe550ffc8c2edf14ea68fcb4c
Patrys123pl <Statnikpatryk@gmail.com>
2023-11-15 09:46:28 +0100

    turret_aktualizacja

--- Assets/Scripts/TurretScript.cs
+++ Assets/Scripts/TurretScript.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class TurretScript : MonoBehaviour
 {
     public Transform player;
+    public GameObject bulletPerfab;
+    public Transform firePoint;
+    public float fireRate = 2f;
+    private float nextTimeToFire = 0f;
     int MaxDist = 15;
     int MinDist = 10;
 
@@ -16,6 +20,24 @@ public class TurretScript : MonoBehaviour
 
     void Update()
     {
-      transform.LookAt(player);
+      if (player != null) 
+        {
+            Vector3 directionToPlayer = player.position - transform.position;
+            directionToPlayer.y = 0;
+            Quaternion lookRotation = Quaternion.LookRotation(directionToPlayer);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+
+            if (Time.time >= nextTimeToFire) 
+            {
+                Shoot();
+                nextTimeToFire = Time.time + 1f / fireRate;
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPerfab, firePoint.position, firePoint.rotation);
+        BulletController bulletController = bullet.GetComponent<BulletController>();
     }
 }
