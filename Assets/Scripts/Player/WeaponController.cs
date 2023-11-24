d5dadb9b56a218016802d640c552c7ac6ea2e46f
Rybon <lukasz2000szabla20@wp.pl>
2023-11-24 19:46:44 +0100

    Ammo UI + Shooting + Realoding

--- Assets/Scripts/Player/WeaponController.cs
+++ Assets/Scripts/Player/WeaponController.cs
@@ -1,17 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponController : MonoBehaviour
 {
     [SerializeField] Transform bulletPrefab;
     [SerializeField] Transform bulletSpawnerTransform;
-    [SerializeField] float weaponCooldown;
-   
-   
-    private void Update(){
-        if(Input.GetMouseButtonDown(0)){
+
+    [SerializeField] private Text ammoText;
+
+    [SerializeField] private int ammo;
+    [SerializeField] private int maxAmmo = 60;
+
+    private bool canShoot = false;
+
+    private void Start()
+    {
+        ammo = maxAmmo;
+    }
+    private void Update()
+    {
+        ShootController();
+        ReloadingController();
+    }
+
+    public void ShootController()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            AmmoController();
+        }
+    }
+
+    public void AmmoController()
+    {
+        canShoot = false;
+        ammoText.text = ammo + "/" + maxAmmo;
+
+        if (ammo > 0)
+        {
+            canShoot = true;
             Instantiate(bulletPrefab, bulletSpawnerTransform.position, bulletSpawnerTransform.rotation);
+            ammo--;
+        }
+        
+        if (ammo >= maxAmmo)
+        {
+            ammo = maxAmmo;
+        }
+    }
+    public void ReloadingController()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            ammo = maxAmmo;
         }
     }
 }
