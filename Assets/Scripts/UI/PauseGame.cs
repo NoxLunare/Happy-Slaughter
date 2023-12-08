b5d5692ccbce42422a0fd12407e8bc0328d701c3
Rybon <lukasz2000szabla20@wp.pl>
2023-12-08 23:00:44 +0100

    ADD to game settings + pause game

--- Assets/Scripts/UI/PauseGame.cs
+++ Assets/Scripts/UI/PauseGame.cs
@@ -1,29 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class PauseGame : MonoBehaviour
 {
     public GameObject scenePauseGame;
+    public GameObject playerUI;
 
     public bool isActive;
 
+    private void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
     private void Update()
     {
         PauseGameController();
+      
     }
     public void PauseGameController()
     {
-        isActive = !isActive;
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            isActive = !isActive;
+            Time.timeScale = isActive ? 0:1;
             scenePauseGame.SetActive(isActive);
+            playerUI.SetActive(!isActive);
         }
     }
 
     public void ResumeGame()
     {
         scenePauseGame.SetActive(!isActive);
+        playerUI.SetActive(isActive);
+        Time.timeScale = 1.0f;
     }
 
     public void BackMainMenu()
