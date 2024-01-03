01cbef52fe663b16d207abd238f6135142c1d27e
Rybon <lukasz2000szabla20@wp.pl>
2024-01-03 11:06:53 +0100

    RoomSpawnScript

--- Assets/Scripts/SpawnRoomController.cs
+++ Assets/Scripts/SpawnRoomController.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class SpawnRoomController : MonoBehaviour
 {
-   
+    public int openingDirection;
+
     void Start()
     {
         
     }
 
+    public void Update()
+    {
+        if (openingDirection == 1)
+        {
+
+        }else if (openingDirection == 2)
+        {
+
+        } else if (openingDirection == 3)
+        {
+
+        }else if(openingDirection == 4)
+        {
+
+        }
+
+    }
   
 }
