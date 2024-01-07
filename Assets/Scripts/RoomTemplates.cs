7f8ece304092c4e18c4fb3ff3fb8c7fa4e177e16
Rybon <lukasz2000szabla20@wp.pl>
2024-01-07 12:50:58 +0100

    SpawnRoom

---
+++ Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public List<GameObject> bottomRoomsList;
+    public List<GameObject> topRoomsList;
+    public List<GameObject> leftRoomsList;
+    public List<GameObject> rightRoomsList;
+}
--- Assets/Scripts/SpawnRoomController.cs
+++ Assets/Scripts/SpawnRoomController.cs
@@ -1,32 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnRoomController : MonoBehaviour
 {
     public int openingDirection;
 
-    void Start()
+    public RoomTemplates templates;
+
+    public Transform roomPosition;
+
+
+    private void Start()
     {
-        
+        SpawnRoom();
     }
 
-    public void Update()
+   public void SpawnRoom()
     {
+      
         if (openingDirection == 1)
         {
-
-        }else if (openingDirection == 2)
-        {
-
-        } else if (openingDirection == 3)
-        {
-
-        }else if(openingDirection == 4)
-        {
-
+            int random = Random.Range(0, templates.bottomRoomsList.Count);
+            Instantiate(templates.bottomRoomsList[random],roomPosition.position,Quaternion.identity);
         }
 
+        if (openingDirection == 2)
+        {
+            int random = Random.Range(0, templates.topRoomsList.Count);
+            Instantiate(templates.topRoomsList[random], roomPosition.position, Quaternion.identity);
+        }
+
+        if(openingDirection == 3)
+        {
+            int random = Random.Range(0, templates.leftRoomsList.Count);
+            Instantiate(templates.leftRoomsList[random], roomPosition.position, Quaternion.identity);
+        }
+
+        if (openingDirection == 4)
+        {
+            int random = Random.Range(0, templates.rightRoomsList.Count);
+            Instantiate(templates.rightRoomsList[random], roomPosition.position, Quaternion.identity);
+        }
     }
-  
+
 }
