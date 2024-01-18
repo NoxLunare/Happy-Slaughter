64f1002ee8867203f3c7d3627ae6efd6b81d7ff0
Patrys123pl <statnikpatryk@gmail.com>
2024-01-18 09:38:24 +0100

    Shooting enemy
    Dodałem działających przeciwników strzelających. tylko że ktoś rozjebał pociski :/
    Chciałem naprawić ale nie wiem jak to naprawić więc zostawiam tak jak jest.

---
+++ Assets/Scripts/AI/EnemyShooting.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float shootCooldown = 2f;
+
+    private float nextShootTime = 0f;
+
+    
+    public void UpdateShooting()
+    {
+        if (Time.time >= nextShootTime)
+        {
+            Shoot();
+            nextShootTime = Time.time + shootCooldown;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(firePoint.forward * 10f, ForceMode.Impulse);
+    }
+}
--- Assets/Scripts/AI/Enemy_Movement.cs
+++ Assets/Scripts/AI/Enemy_Movement.cs
@@ -1,42 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 public class Enemy_Movement : MonoBehaviour
 {
-
     public Transform player;
-    int MoveSpeed = 4;
-    int MaxDist = 10;
-    int MinDist = 1;
+    public int moveSpeed = 4;
+    public int maxDist = 10;
+    public int minDist = 1;
 
+    private EnemyShooting enemyShooting; 
 
     void Start()
     {
-        player = GameObject.Find("Player").GetComponent<Transform>();
+        player = GameObject.Find("Player").transform;
+        enemyShooting = GetComponent<EnemyShooting>(); 
     }
 
     void Update()
     {
-        //transform.LookAt(player);
-
         Vector3 directionToPlayer = player.position - transform.position;
         directionToPlayer.y = 0;
         Quaternion lookRotation = Quaternion.LookRotation(directionToPlayer);
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
 
-        if (Vector3.Distance(transform.position, player.position) >= MinDist)
+        if (Vector3.Distance(transform.position, player.position) >= minDist)
         {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
 
-            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
-
-
-
-            if (Vector3.Distance(transform.position, player.position) <= MaxDist)
+            if (Vector3.Distance(transform.position, player.position) <= maxDist)
             {
-                //Here Call any function U want Like Shoot at here or something
+                enemyShooting.UpdateShooting();
             }
-
         }
     }
 }
